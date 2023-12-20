1f96b58201f50e35ab45c049d38f02e4264a3ae6
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-12-20 12:00:40 +0100

    fix(libraryFaction) UI

--- DuelOfReigns/src/components/UI/LibraryFaction/LibraryFaction.jsx
+++ DuelOfReigns/src/components/UI/LibraryFaction/LibraryFaction.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import {Container, Row, Col, Button} from 'reactstrap';
-import { Link } from "react-router-dom";
+import {Container, Row, Col, Button, Card, CardImg, CardBody, CardTitle} from 'reactstrap';
+import {Link} from "react-router-dom";
 
 import demonImg from '../../../assets/images/card-demon-placeholder.png';
 import orcImg from '../../../assets/images/card-orc-placeholder.png';
 import elveImg from '../../../assets/images/card-elves-placeholder.png';
 import humanImg from '../../../assets/images/card-human-placeholder.png';
-
-import SkinCard from "../SkinCard/SkinCard";
 
 import './LibraryFaction.css';
 
@@ -19,49 +17,100 @@ const LibraryFaction = () => {
                     <div className="live__auction__top d-flex align-items-center justify-content-between">
                         <h2>Factions</h2>
                     </div>
-                    <p className="text-right">Il existe plusieurs factions au sein de Duel Reigns. Chacune d'entre elles apporte sa propre perspective, ses motivations et son rôle unique. Ces factions peuvent former des alliances ou se lancer dans des affrontements épiques. Voici un aperçu des principales factions que vous pouvez rencontrer.</p>
+                    <p className="text-right">Il existe plusieurs factions au sein de Duel Reigns. Chacune d'entre elles
+                        apporte sa propre perspective, ses motivations et son rôle unique. Ces factions peuvent former
+                        des alliances ou se lancer dans des affrontements épiques. Voici un aperçu des principales
+                        factions que vous pouvez rencontrer.</p>
                 </Col>
 
                 <Col lg='3' md='4' sm='6'>
-                    <div className="single__hero__card">
-                        <div className="card__img">
-                            <img src={demonImg} className="w-100"/>
-                        </div>
-                        <div className="card__content">
-                            <h5 className="card__title text-center">Démons</h5>
-                        </div>
-                    </div>
+                    <Card
+                        className="single__hero__card"
+                        style={{border: "none"}}
+                    >
+                        <CardImg top src={demonImg} className="w-100" alt="Démons"/>
+
+                        <CardBody>
+                            <CardTitle tag="h5" className="card__title text-center">Démons</CardTitle>
+                        </CardBody>
+                    </Card>
                 </Col>
                 <Col lg='3' md='4' sm='6'>
-                    <div className="single__hero__card">
-                        <div className="card__img">
-                            <img src={orcImg} className="w-100"/>
-                        </div>
-                        <div className="card__content">
-                            <h5 className="card__title text-center">Orcs</h5>
-                        </div>
-                    </div>
+                    <Card
+                        className="single__hero__card"
+                        style={{border: "none"}}
+                    >
+                        <CardImg top src={orcImg} className="w-100" alt="Orcs"/>
+                        <CardBody>
+                            <CardTitle tag="h5" className="card__title text-center">Orcs</CardTitle>
+                        </CardBody>
+                    </Card>
                 </Col>
                 <Col lg='3' md='4' sm='6'>
-                    <div className="single__hero__card">
-                        <div className="card__img">
-                            <img src={elveImg} className="w-100"/>
-                        </div>
-                        <div className="card__content">
-                            <h5 className="card__title text-center">Elfes</h5>
-                        </div>
-                    </div>
+                    <Card
+                        className="single__hero__card"
+                        style={{border: "none"}}
+                    >
+                        <CardImg top src={elveImg} className="w-100" alt="Elfes"/>
+                        <CardBody>
+                            <CardTitle tag="h5" className="card__title text-center">Elfes</CardTitle>
+                        </CardBody>
+                    </Card>
                 </Col>
                 <Col lg='3' md='4' sm='6'>
-                    <div className="single__hero__card">
-                        <div className="card__img">
-                            <img src={humanImg} className="w-100"/>
-                        </div>
-                        <div className="card__content">
-                            <h5 className="card__title text-center">Humains</h5>
-                        </div>
-                    </div>
+                    <Card
+                        className="single__hero__card"
+                        style={{border: "none"}}
+                    >
+                        <CardImg top src={humanImg} className="w-100" alt="Humains"/>
+                        <CardBody>
+                            <CardTitle tag="h5" className="card__title text-center">Humains</CardTitle>
+                        </CardBody>
+                    </Card>
                 </Col>
+
+                {/*<Col lg='3' md='4' sm='6'>*/}
+                {/*    <div className="single__hero__card">*/}
+                {/*        <div className="card__img">*/}
+                {/*            <img src={demonImg} className="w-100"/>*/}
+                {/*        </div>*/}
+                {/*        <div className="card__content">*/}
+                {/*            <h5 className="card__title text-center">Démons</h5>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</Col>*/}
+                {/*<Col lg='3' md='4' sm='6'>*/}
+                {/*    <div className="single__hero__card">*/}
+                {/*        <div className="card__img">*/}
+                {/*            <img src={orcImg} className="w-100"/>*/}
+                {/*        </div>*/}
+                {/*        <div className="card__content">*/}
+                {/*            <h5 className="card__title text-center">Orcs</h5>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</Col>*/}
+                {/*<Col lg='3' md='4' sm='6'>*/}
+                {/*    <div className="single__hero__card">*/}
+                {/*        <div className="card__img">*/}
+                {/*            <img src={elveImg} className="w-100"/>*/}
+                {/*        </div>*/}
+                {/*        <div className="card__content">*/}
+                {/*            <h5 className="card__title text-center">Elfes</h5>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</Col>*/}
+                {/*<Col lg='3' md='4' sm='6'>*/}
+                {/*    <div className="single__hero__card">*/}
+                {/*        <div className="card__img">*/}
+                {/*            <img src={humanImg} className="w-100"/>*/}
+                {/*        </div>*/}
+                {/*        <div className="card__content">*/}
+                {/*            <h5 className="card__title text-center">Humains</h5>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</Col>*/}
+
+
                 <Col lg='12'>
                     <Button
                         color={"secondary"}
--- DuelOfReigns/src/pages/HomePage.jsx
+++ DuelOfReigns/src/pages/HomePage.jsx
@@ -4,10 +4,8 @@ import LibraryFaction from "../components/UI/LibraryFaction/LibraryFaction";
 import RankSection from "../components/UI/RankSection/RankSection";
 import TrendingSkinSection from "../components/UI/TrendingSkinSection/TrendingSkinSection";
 import NewsSection from "../components/UI/NewsSection/NewsSection";
-import {Trans, useTranslation} from "react-i18next";
 
 const HomePage = () => {
-    const {t, i18n} = useTranslation();
     return (<>
         <HeroSection/>
         <LibraryFaction/>
