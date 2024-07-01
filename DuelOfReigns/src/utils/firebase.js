abc0f9cf75adef53d012a15884634723bdcbfeb0
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2024-07-01 12:27:09 +0200

    fix(deploy): firebase config

--- DuelOfReigns/src/utils/firebase.js
+++ DuelOfReigns/src/utils/firebase.js
@@ -1,11 +1,14 @@
 // Initializes and configures Firebase, providing an asynchronous function getAppInstance to retrieve the Firebase app instance.
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 
 // Firebase configuration
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY || process.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ||process.env.VITE_FIREBASE_PROJECT_ID,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID || process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialiser Firebase
