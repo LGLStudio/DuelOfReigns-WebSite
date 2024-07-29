c98c42f6533f76c9046a7ac0ae30fcbc49dfad5b
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2024-07-29 17:15:55 +0200

    feat(marketPlace): display skins on sales

---
+++ DuelOfReigns/src/TODO.jsx
@@ -0,0 +1,27 @@
+const TODO = () => {
+    return (<>
+        1) Lucas achète un skin sur la boutique
+        - vérifier qu'on écrit bien toutes les données nécessaire en base de données
+
+        2) Lucas mets en vente un skin sur le market place
+        a) connecter Lucas sur le site
+        b) récupérer tous les skins de Lucas
+        - lister tous les skin_sales pour séparer les skins en ventes des skins pas en ventes
+        c) mettre en vente un skin
+        - créer une entrée sale_skin avec les infos nécessaires
+        - actualiser la page pour re-lister les skins en vente
+
+
+        3) Louis achète le skin sur le market place
+        - Vérification du nombre d'écopoco que Louis possède
+        - Débite les Ecopoco de Louis de (Skin_Sale.price + Skin_Sale.price*Skin_Sale.Fee)
+        - Crédite les Ecopoco de Lucas de Skin_Sale.price
+        - Edite Skin_Sale :
+        - Ajoute l'entrée User_Buyer
+        - Ajoute l'entrée Date_Transaction
+        - Edite le skin dont Skin.id === Skin_Sale.Skin
+        - change le Owner
+    </>)
+}
+
+export default TODO
--- DuelOfReigns/src/pages/MarketPage.jsx
+++ DuelOfReigns/src/pages/MarketPage.jsx
@@ -1,44 +1,86 @@
 import BlockHeader from "../components/UI/BlockHeader/BlockHeader.jsx";
 import SkinCard from "../components/UI/SkinCard/SkinCard.jsx";
 import {useAuth} from "../AuthProvider.jsx";
+import {Button, Spinner} from "reactstrap";
+import {useNavigate} from "react-router-dom";
+import ecopocoHead from "../../src/assets/images/ecopoco_face-removebg.png";
+import ecopocoTail from "../../src/assets/images/ecopoco_pile-removebg.png";
+import {doc, getDoc, getDocs, getFirestore, collection} from "firebase/firestore";
+import {useEffect, useState} from "react";
 
 const MarketPage = () => {
     const auth = useAuth();
     const fakeItems = []
-    for (let i = 0; i < 10; i++) {
-        fakeItems.push(
-            {
-                name: `nom_${i + 1}`,
-                price: `${i + Math.floor(Math.random() * 10)}`,
-                date: `${new Date().toLocaleDateString("fr")}`,
-                id: `${i}`
+    const navigate = useNavigate();
+    const user = auth.user
+    const [skins, setSkins] = useState([]);
+    const db = getFirestore();
+
+    useEffect(() => {
+        const getSkins = async () => {
+            try {
+                const skinsRef = collection(db, 'skin_sales');
+                const querySnapshot = await getDocs(skinsRef);
+
+                const skinsList = await Promise.all(querySnapshot.docs.map(async (docSnapshot) => {
+                    const data = docSnapshot.data();
+
+                    // Fetch skin details
+                    const skinDoc = await getDoc(data.skin);
+                    const skinData = skinDoc.exists() ? skinDoc.data() : null;
+
+                    // Fetch user seller details
+                    const userSellerDoc = await getDoc(data.user_seller);
+                    const userSellerData = userSellerDoc.exists() ? userSellerDoc.data() : null;
+
+                    return {
+                        ...data,
+                        skin: skinData,
+                        user_seller: userSellerData
+                    };
+                }));
+
+                setSkins(skinsList);
+            } catch (error) {
+                console.error("Error fetching skins for sale: ", error);
             }
-        )
-    }
+        };
+
+        getSkins().then();
+    }, [db]);
+
     return (
         <>
             <BlockHeader/>
             <div style={{display: "flex", justifyContent: "space-evenly"}}>
                 <div>MarketPlace</div>
-                <div>Vendre un item</div>
+                <Button
+                    onClick={() => navigate("/sell")}
+                >
+                    Vendre un item
+                </Button>
             </div>
             <div>
                 {auth?.user ?
-                    <>Mes ecopocos : {auth.user.coins} </>
+                    <div>Mes ecopocos : {auth.user.coins} <img width={20} src={ecopocoTail} alt={"ecopoco-icon"}/></div>
                     : <></>}
             </div>
             Les différents cosmétiques en ventes :
             <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
-                {fakeItems.map(item => (
-                    <div style={{
-                        width: "200px",
-                        margin: "1rem"
-                    }}
-                         key={item.id}
-                    >
-                        <SkinCard item={item}/>
-                    </div>
-                ))}
+                {skins ?
+                    <>
+                        {skins.map(item => (
+                            <div style={{
+                                width: "200px",
+                                margin: "1rem"
+                            }}
+                                 key={Math.random()}
+                            >
+                                <SkinCard item={item}/>
+                            </div>
+                        ))}
+                    </>
+                    : <Spinner/>}
             </div>
         </>
     )
