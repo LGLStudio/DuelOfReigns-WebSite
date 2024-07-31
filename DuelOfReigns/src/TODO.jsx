6a0d0548fbf61841723443c2ce5abf440b0d5105
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2024-07-31 16:42:33 +0200

    fix(Skin): error handling

--- DuelOfReigns/src/TODO.jsx
+++ DuelOfReigns/src/TODO.jsx
@@ -1,15 +1,9 @@
 const TODO = () => {
     return (<>
-        1) Lucas achète un skin sur la boutique
-        - vérifier qu'on écrit bien toutes les données nécessaire en base de données
-
         2) Lucas mets en vente un skin sur le market place
-        b) récupérer tous les skins_sales de Lucas
-        - lister tous les skin_sales pour séparer les skins en ventes des skins pas en ventes
-        c) mettre en vente un skin
-        - créer une entrée sale_skin avec les infos nécessaires
-        - actualiser la page pour re-lister les skins en vente
-
+        b) récupérer tous les skins de Lucas
+        - Afficher une liste avec les skins_sales deja en vente
+        - Afficher une liste avec les skins possédés mais non en vente
 
         3) Louis achète le skin sur le market place
         - Vérification du nombre d'écopoco que Louis possède
